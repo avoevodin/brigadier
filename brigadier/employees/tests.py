65d899ff2502e071fdf7deac6740bca368c06960
Andrey Voevodin <avoevodin8888@gmail.com>
2021-03-27 23:28:23 +0800

    * added not working tests * added study plan * removed __init__.py from projects/brigadier/brigadier * #2

--- brigadier/employees/tests.py
+++ brigadier/employees/tests.py
@@ -3,15 +3,35 @@ from django.test import TestCase
 from .models import Employee
 
 
-def CreateEmployee(**kwargs):
+def create_employee(**kwargs):
     """Create an employee with passed parameters.
 
     """
-    return Employee.objects.create(
-        kwargs.get('firstname'),
-        kwargs.get('middlename'),
-        kwargs.get('surname'),
-        kwargs.get('firstname'),
-        kwargs.get('email'),
-        kwargs.get('birthdate'),
-    )
+    try:
+        return Employee.objects.create(
+            firstname=kwargs.get('firstname'),
+            middlename=kwargs.get('middlename'),
+            surname=kwargs.get('surname'),
+            email=kwargs.get('email'),
+            birthdate=kwargs.get('birthdate'),
+        )
+    except ValueError:
+        return ValueError
+
+
+class EmployeeModelTest(TestCase):
+
+    def test_empty_first_name(self):
+        """todo()
+
+        """
+        result = 1
+        # try:
+        #     create_employee(kwargs={
+        #         'middlename': 'test',
+        #
+        #     })
+        # except ValueError:
+        #     result = 0
+
+        self.assertEqual(0, 0)
--- brigadier/projects/tests.py
+++ brigadier/projects/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ProjectModelTest(TestCase):
+    def just_test(self):
+        self.assertEqual(1, 1)

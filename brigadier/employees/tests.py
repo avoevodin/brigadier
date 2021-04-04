1aab020cf6a84bdf936a85fc6e6308dca31ef2ec
Andrey Voevodin <avoevodin8888@gmail.com>
2021-04-04 23:52:07 +0800

    * added tests for Employee model * #8

--- brigadier/employees/tests.py
+++ brigadier/employees/tests.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
 
 from .models import Employee
 
@@ -7,31 +10,109 @@ def create_employee(**kwargs):
     """Create an employee with passed parameters.
 
     """
-    try:
-        return Employee.objects.create(
-            firstname=kwargs.get('firstname'),
-            middlename=kwargs.get('middlename'),
-            surname=kwargs.get('surname'),
-            email=kwargs.get('email'),
-            birthdate=kwargs.get('birthdate'),
-        )
-    except ValueError:
-        return ValueError
+    return Employee.objects.create(
+        firstname=kwargs.get('firstname'),
+        middlename=kwargs.get('middlename'),
+        surname=kwargs.get('surname'),
+        email=kwargs.get('email'),
+        birthdate=kwargs.get('birthdate'),
+    )
 
 
 class EmployeeModelTest(TestCase):
 
-    def test_empty_first_name(self):
-        """todo()
+    def test_full_name(self):
+        """todo
 
         """
-        result = 1
-        # try:
-        #     create_employee(kwargs={
-        #         'middlename': 'test',
-        #
-        #     })
-        # except ValueError:
-        #     result = 0
+        firstname = 'Marshall'
+        middlename = 'Bruce'
+        surname = 'Mathers'
+        email = 'mbm@example.com'
+        birthdate = timezone.now()
+        full_name = create_employee(**{
+            'firstname': firstname,
+            'middlename': middlename,
+            'surname': surname,
+            'email': email,
+            'birthdate': birthdate,
+        }).full_name()
+        full_name_target = 'Marshall Bruce Mathers'
+        self.assertEqual(full_name, full_name_target)
 
-        self.assertEqual(0, 0)
+    def test_full_name_without_firstname(self):
+        """todo
+
+        """
+        firstname = ''
+        middlename = 'Bruce'
+        surname = 'Mathers'
+        email = 'mbm@example.com'
+        birthdate = timezone.now()
+        full_name = create_employee(**{
+            'firstname': firstname,
+            'middlename': middlename,
+            'surname': surname,
+            'email': email,
+            'birthdate': birthdate,
+        }).full_name()
+        full_name_target = 'Bruce Mathers'
+        self.assertEqual(full_name, full_name_target)
+
+    def test_full_name_without_middlename(self):
+        """todo
+
+        """
+        firstname = 'Marshall'
+        middlename = None
+        surname = 'Mathers'
+        email = 'mbm@example.com'
+        birthdate = timezone.now()
+        full_name = create_employee(**{
+            'firstname': firstname,
+            'middlename': middlename,
+            'surname': surname,
+            'email': email,
+            'birthdate': birthdate,
+        }).full_name()
+        full_name_target = 'Marshall Mathers'
+        self.assertEqual(full_name, full_name_target)
+
+    def test_full_name_without_surname(self):
+        """todo
+
+        """
+        firstname = 'Marshall'
+        middlename = 'Bruce'
+        surname = ''
+        email = 'mbm@example.com'
+        birthdate = timezone.now()
+        full_name = create_employee(**{
+            'firstname': firstname,
+            'middlename': middlename,
+            'surname': surname,
+            'email': email,
+            'birthdate': birthdate,
+        }).full_name()
+        full_name_target = " ".join([firstname or '', middlename or '', surname or '']) \
+            .replace("  ", " ").strip()
+        self.assertEqual(full_name, full_name_target)
+
+    def test_full_name_without_any_name(self):
+        """todo
+
+        """
+        firstname = ''
+        middlename = None
+        surname = ''
+        email = 'mbm@example.com'
+        birthdate = timezone.now()
+        full_name = create_employee(**{
+            'firstname': firstname,
+            'middlename': middlename,
+            'surname': surname,
+            'email': email,
+            'birthdate': birthdate,
+        }).full_name()
+        full_name_target = ''
+        self.assertEqual(full_name, full_name_target)

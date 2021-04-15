69309dfc11e55c3a14ffa353ea7d2101510c0f2f
Andrey Voevodin <avoevodin8888@gmail.com>
2021-04-16 00:52:54 +0800

    * updated creation view and template with adding user to group * #18

--- brigadier/accounts/views.py
+++ brigadier/accounts/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.views import FormView
+from django.contrib.auth.models import Group
+from django.urls import reverse
 from .forms import RegistrationForm
+from django.conf import settings
 
 
 class RegistrationView(FormView):
@@ -8,4 +11,15 @@ class RegistrationView(FormView):
     """
     form_class = RegistrationForm
     template_name = 'registration.html'
+    success_url = settings.LOGIN_REDIRECT_URL
 
+    def form_valid(self, form):
+        response = super(RegistrationView, self).form_valid(form)
+        user = form.save()
+        try:
+            group_public = Group.objects.get(name='public')
+        except Group.DoesNotExist:
+            group_public = None
+        if group_public is not None:
+            user.groups.add(group_public)
+        return response

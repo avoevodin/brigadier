f6894f1569624278b768644320ecb46e23bdbf54
avo888 <avoevodin8888@gmail.com>
2021-05-04 22:05:43 +0300

    * updated user admin form with removing date_of_birth #28

--- brigadier/accounts/admin.py
+++ brigadier/accounts/admin.py
@@ -69,8 +69,8 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'date_of_birth', 'password1', 'password2')
-        })
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
     )
     search_fields = ('username', 'email',)
     ordering = ('email',)

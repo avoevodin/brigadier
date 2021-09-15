5c35a61b09ec736455997d6af6642c24bb45621e
avo888 <avoevodin8888@gmail.com>
2021-09-15 22:34:08 +0300

    * updated test for verification email * this commit needs to fix pycharm bug #32

--- brigadier/worker/tests.py
+++ brigadier/worker/tests.py
@@ -1,4 +1,9 @@
+from uuid import uuid4
+
 from django.test import TestCase
+from .email.tasks import send_verification_mail
+from django.conf import settings
+from django.core import mail
 
 
 class WorkerEmailTest(TestCase):
@@ -10,4 +15,13 @@ class WorkerEmailTest(TestCase):
         """todo
 
         """
-        
+        email = 'test@example.com'
+        key = uuid4().hex
+        confirm = uuid4().hex
+        host = 'testserver'
+        send_verification_mail(host, email, key, confirm)
+        self.assertEqual(len(mail.outbox), 1)
+        activation_mail = mail.outbox[0]
+        self.assertEqual(activation_mail.subject, 'Activate your email.')
+        self.assertEqual(activation_mail.from_email, settings.DEFAULT_FROM_EMAIL)
+        self.assertEqual(activation_mail.to, [email])
